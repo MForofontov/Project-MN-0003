888e221a05bdcf57b749015448c4e242d07134ca
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-28 14:00:29 +0000

    feat: Added docker Files and docker-compose

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -186,8 +186,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis_mn_003_docker:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis_mn_003_docker:6379/0'
 CELERY_TIMEZONE = 'UTC'
 CELERY_ENABLE_UTC = True
 CELERY_ACCEPT_CONTENT = ['json']
