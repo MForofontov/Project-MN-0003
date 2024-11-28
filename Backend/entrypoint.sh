98eabed43d8f522b41a8f4289ef17ffea52e09dc
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-28 15:27:15 +0000

    feat: Added docker Files and docker-compose

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -7,8 +7,18 @@
 python manage.py makemigrations
 python manage.py migrate
 
-# Create a superuser
-python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+# Create a superuser if it doesn't exist
+echo "Creating superuser"
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+import os;
+User = get_user_model();
+if not User.objects.filter(email=os.environ.get('DJANGO_SUPERUSER_EMAIL')).exists():
+    User.objects.create_superuser(
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    )
+"
 
 # Start the Django development server
 python manage.py runserver 0.0.0.0:8000
