aab5fcc9a93d70e48bb9ab641af66d54cfac4531
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-28 13:36:51 +0000

    feat: Modified backend

--- Backend/api/tasks.py
+++ Backend/api/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
 import time  # Example for simulating a long process
-from django.core.files.base import ContentFile
 from django.db import transaction
 from reports.models import Report
 from music.models import MusicFile
@@ -24,7 +23,7 @@ def process_file_task(file_path: str) -> None:
     print(f"Processing file: {file_path}")
     
     # Simulate a long process
-    time.sleep(10)
+    time.sleep(30)
     
     print(f"Finished processing file: {file_path}")
 
@@ -36,20 +35,16 @@ def process_file_task(file_path: str) -> None:
         return
 
     # Generate the report content
-    report_content = f"Analysis of {music_file.title}\n"
-    report_content += "This is a placeholder for the actual analysis.\n"
-
-    # Create a ContentFile from the report content
-    report_file = ContentFile(report_content.encode('utf-8'))
-
+    report_content = f"Analysis of {music_file.title} ran sucessfuly\n"
+    
     # Create a new Report instance and save the report file
     with transaction.atomic():
         report = Report(
             user=music_file.user,
             music_file=music_file,
+            result=report_content,
             title=f"Report for {music_file.title}"
         )
-        report.file.save(f'report_{music_file.id}.txt', report_file)
         report.save()
 
     print(f"Report generated and saved for file: {file_path}")
--- Backend/reports/models.py
+++ Backend/reports/models.py
@@ -4,25 +4,6 @@ from music.models import MusicFile
 
 User = get_user_model()
 
-def report_directory_path(instance: 'Report', filename: str) -> str:
-    """
-    Generate a file path for a new report file.
-
-    Parameters
-    ----------
-    instance : Report
-        The instance of the Report model.
-    filename : str
-        The original filename of the uploaded file.
-
-    Returns
-    -------
-    str
-        The generated file path.
-    """
-    # file will be uploaded to MEDIA_ROOT/user_<id>/reports/<filename>
-    return 'user_{0}/reports/{1}'.format(instance.user.id, filename)
-
 class Report(models.Model):
     """
     Model representing a report uploaded by a user.
@@ -30,7 +11,7 @@ class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports')
     music_file = models.ForeignKey(MusicFile, on_delete=models.CASCADE, related_name='reports')
     title = models.CharField(max_length=255)
-    file = models.FileField(upload_to=report_directory_path)
+    result = models.TextField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
