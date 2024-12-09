a09bcd339235c8e501551d5130402707006cf811
MForofontov <mykyta.forofontov@hotmail.com>
2024-12-09 14:14:19 +0000

    feat: started wotking on micro services architecture

--- data_processing_service/data_processing_service/urls.py
+++ data_processing_service/data_processing_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("process_music_files/", include("process_music_files.urls")),
+    path("api/", include("api.urls")),
 ]
