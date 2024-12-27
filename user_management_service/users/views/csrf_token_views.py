f7995b6788d1b0ab4b3de1c87174288212eada20
MForofontov <mykyta.forofontov@hotmail.com>
2024-12-27 14:35:01 +0000

    docs: adding docs

--- user_management_service/users/views/csrf_token_views.py
+++ user_management_service/users/views/csrf_token_views.py
@@ -1,6 +1,23 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
-def get_csrf_token(request):
+def get_csrf_token(request: HttpRequest) -> JsonResponse:
+    """
+    View to set the CSRF token in the client's cookies.
+
+    This view is decorated with ensure_csrf_cookie to ensure that a CSRF token
+    is set in the client's cookies. It returns a JSON response indicating that
+    the CSRF token has been set.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The HTTP request object.
+
+    Returns
+    -------
+    JsonResponse
+        A JSON response indicating that the CSRF token has been set.
+    """
     return JsonResponse({'message': 'CSRF token set'})
