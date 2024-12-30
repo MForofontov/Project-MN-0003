17699571f7f13714e54cc7e35481583d40d970b1
MForofontov <mykyta.forofontov@hotmail.com>
2024-12-30 15:49:55 +0000

    feat: working on email system

---
+++ user_management_service/users/tasks/send_verification_email.py
@@ -0,0 +1,32 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from users.models import CustomUser
+from users.tokens import email_verification_token
+
+@shared_task
+def send_verification_email(user_id):
+    """
+    Sends a verification email to the user.
+
+    Parameters
+    ----------
+    user_id : int
+        The ID of the user to send the verification email to.
+    """
+    try:
+        user = CustomUser.objects.get(pk=user_id)
+        token = email_verification_token.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        verification_link = f"{settings.FRONTEND_URL}/verify-email/{uid}/{token}/"
+        subject = "Verify your email address"
+        message = render_to_string('email_verification.html', {
+            'user': user,
+            'verification_link': verification_link,
+        })
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
+    except CustomUser.DoesNotExist:
+        pass
