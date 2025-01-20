8169311b9cf3624fb41b07820b5ac491a074085a
MForofontov <mykyta.forofontov@hotmail.com>
2025-01-20 18:09:44 +0000

    refactor: replaced zookeeper with KRaft

--- user_management_service/entrypoint.sh
+++ user_management_service/entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 # Wait for the database to be ready
-/user_management_service_docker/wait-for-it.sh db_user_management_service:5432 --timeout=60 --strict -- echo "Database is up"
+/user_management_service_docker/wait-for-it.sh db_user_management_service:5432 --timeout=60 --strict -- echo "[INFO] Database is up for user_management_service"
 
 # Wait for Redis to be ready
-/user_management_service_docker/wait-for-it.sh redis_user_management_service:6379 --timeout=60 --strict -- echo "Redis is up"
+/user_management_service_docker/wait-for-it.sh redis_user_management_service:6379 --timeout=60 --strict -- echo "[INFO] Redis is up for user_management_service"
+
+# Wait for Kafka to be ready
+/user_management_service_docker/wait-for-it.sh kafka_user_management_service:9092 --timeout=60 --strict -- echo "[INFO] Kafka is up for user_management_service"
 
 # Run migrations
+echo "[INFO] Running migrations for user_management_service"
 python manage.py makemigrations
 python manage.py migrate
 
 # Create a superuser if it doesn't exist
-echo "Creating superuser"
+echo "[INFO] Creating superuser"
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 import os;
@@ -24,4 +28,5 @@ if not User.objects.filter(email=os.environ.get('DJANGO_SUPERUSER_EMAIL')).exist
 "
 
 # Start the Django development server
+echo "[INFO] user_management_service is up and running"
 python manage.py runserver 0.0.0.0:8000
